ee1e411642943cf40311dddd12d1d07782e6f5db
caspi46 <68092667+caspi46@users.noreply.github.com>
2022-09-19 15:06:41 -0700

    Add files via upload

--- WebScrape.py
+++ WebScrape.py
@@ -1,19 +1,14 @@
-import sqlite3
-import requests
-from bs4 import BeautifulSoup
-import matplotlib.pyplot as plt
-import threading
-from sklearn.linear_model import LinearRegression
-import pandas as pd
-from pylab import *
-from collections import namedtuple
+
 from GraphCreator import GraphCreator
 from SaveSQL import *
 from requests_html import HTMLSession
-
+import requests
+from bs4 import BeautifulSoup
+import threading
+import json
 # This class webscrape for DAZADAZ project.
 # In read,
-# contens -> the video link & save the link the self.link
+# contents -> the video link & save the link the self.link
 # -> title, views, date, comments, likes & save them in the lists
 # save the data into sql
 
@@ -22,16 +17,17 @@ class Webscrape:
     def __init__(self):
         self.url = "https://www.youtube.com/c/Dazbeeee/videos"
         self.session = HTMLSession()
-        self.title = [ ]
         self.song_title = [ ]
         self.view = [ ]
         self.like = [ ]
         self.date = [ ]
         self.link = [ ]
         self.total = [ ]
-        self.view_title = [ ]
         self.totalView = 0
         self.info = [ ]
+        self.view_title_link = [ ]
+        self.like_title_link = [ ]
+        self.top25_like = [ ]
         self.top25_view = [ ]
 
     def readVideoList(self):
@@ -45,18 +41,25 @@ class Webscrape:
             thd.join()
         self.setTotal()
         self.setViewTitle()
+        self.totalView = sum(self.view)
+        self.setLikeTitle()
+
+    def videoLikes(self, i):
+        r = requests.get(i, headers={'User-Agent': ''})
+        likes = r.text[:r.text.find(' likes"')]
+        like_num = int(likes[likes.rfind('"') + 1:].replace(',', ""))
+        self.like.append(like_num)
 
     def check(self, i):
         condition.acquire()
         res = requests.get(i)
         soup = BeautifulSoup(res.text, 'lxml')
-        title = str(soup.find("meta", itemprop="name")["content"])
-        if self.splitTitle(title):
+        video_title = soup.find("meta", itemprop="name")["content"]
+        if self.splitTitle(video_title):
             self.link.append(i)
-            self.title.append(str(soup.find("meta", itemprop="name")["content"]))
+            self.videoLikes(i)
             self.date.append(str(soup.find("meta", itemprop="datePublished")["content"]))
             self.view.append(int(soup.find("meta", itemprop="interactionCount")["content"]))
-
         condition.release()
 
 
@@ -65,8 +68,9 @@ class Webscrape:
         sq = sqlite()
         sq.connect()
         sq.table("Database")
-        for i in range(len(self.title)):
-            sql_thd = threading.Thread(target=sq.insert, args=(i + 1, self.title[i], self.view[i],
+        for i in range(len(self.song_title)):
+            sql_thd = threading.Thread(target=sq.insert, args=(i + 1, self.song_title[i], self.info[i],
+                                                               self.view[i], self.like[i],
                                                                self.date[i], self.link[i]))
             sql_thd.start()
             sql_thd.join()
@@ -74,19 +78,33 @@ class Webscrape:
 
     def gr(self):
         self.set_top25_view()
-        gc = GraphCreator(self.view_title)
+        self.set_top25_like()
+        gc = GraphCreator(self.top25_view, self.top25_like)
         gc.createTable()
 
     def setTotal(self):
-        for i in range(len(self.title)):
-            self.total.append((self.view[i], self.song_title[i], self.title[i], self.date[i], self.info[i], self.link[i]))
+        for i in range(len(self.song_title)):
+            self.total.append((self.view[i], self.song_title[i], self.like[i], self.date[i], self.info[i], self.link[i]))
 
     def setViewTitle(self):
-        for i in range(len(self.title)):
-            self.view_title.append((self.view[i], self.song_title[i]))
+        for i in range(len(self.song_title)):
+            self.view_title_link.append((self.view[i], self.song_title[i], self.link[i]))
+
+    def setLikeTitle(self):
+        for i in range(len(self.song_title)):
+            self.like_title_link.append((self.like[i], self.song_title[i], self.link[i]))
+
+    def set_top25_like(self):
+        self.like_title_link.sort()
+        for i in range(25):
+            info_data = self.like_title_link[len(self.view_title_link) - i - 1]
+            self.top25_like.append(info_data)
 
     def set_top25_view(self):
-        self.view_title.sort()
+        self.view_title_link.sort()
+        for i in range(25):
+            info_data = self.view_title_link[len(self.view_title_link) - i - 1]
+            self.top25_view.append(info_data)
 
 
     def checkExcludes(self, title):
@@ -106,9 +124,9 @@ class Webscrape:
         ta = t
         if self.checkExcludes(ta):
             return False
-        if t.find('M/V') != -1:  # + 'Official Piano Arrange
-            self.info.append("Original")
-            e = re.sub(remove, '', t.split(" | ")[1])
+        if t.find('M/V') != -1: # + 'Official Piano Arrange
+            self.info.append("ORIGINAL")
+            e = re.sub(remove, '', t.split(" | ")[1]).replace('M/V', "")
             self.song_title.append(e)
             return True
 
@@ -116,21 +134,14 @@ class Webscrape:
             artist = re.findall(remain, t)[0]
             self.info.append(artist)
         else:
-            self.info.append("NONE")
+            self.info.append("UNKNOWN")
 
         for j in excludes2:
             if t.find(j) != -1:
                 e = re.sub(remove, '', t).split(j)
                 self.song_title.append(e[0])
                 return True
-
-        return False
-
-
-
-
-
-# '未来古代楽団 feat. DAZBEE 「はじまりのまえ、おしまいのあと」【Official Video】'
-# 'プラスチック  PLUS+ ／ダズビー COVER  -2014'
+        self.song_title.append(re.sub(remove, '', t))
+        return True
 
 
