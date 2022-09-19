7e40035dd52dc0e8e2351a87a5b15cb896ee2e05
caspi46 <68092667+caspi46@users.noreply.github.com>
2022-09-19 15:28:17 -0700

    Update GraphCreator.py

--- GraphCreator.py
+++ GraphCreator.py
@@ -3,25 +3,32 @@ import plotly.graph_objects as px
 import plotly.graph_objects as go
 #import webbrowser
 
+# GraphCreator
+# Create Bar Chart (Top 25 views & likes)
 class GraphCreator:
     def __init__(self, view_title_link, like_title_link):
+        # views info
         self.view = [ ]
         self.titleV = [ ]
-        self. num = [ ]
+        # likes info
         self.like = [ ]
         self.titleL = [ ]
-
+        
+        # Set views & likes info
         for i in range(25):
-            self.num.append("#" + str(i + 1))
             self.view.append(view_title_link[i][0])
             self.titleV.append(view_title_link[i][1])
             self.like.append(like_title_link[i][0])
             self.titleL.append(like_title_link[i][1])
-        self.view_di = dict(zip(self.num, view_title_link))
-        self.like_di = dict(zip(self.num, like_title_link))
+            
+        #self.view_di = dict(zip(self.num, view_title_link))
+        #self.like_di = dict(zip(self.num, like_title_link))
 
 
-
+    # createTable
+    # create bar graph table
+    # includes two version (views & likes)
+    # two buttons (views & likes)
     def createTable(self):
         plot = px.Figure(data=[go.Bar(
             name='VIEW',
